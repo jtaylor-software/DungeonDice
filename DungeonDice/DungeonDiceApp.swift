45ebfe6777efd5678b7115944e4c4c01f42e2d61
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-07 13:16:00 -0600

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Jeremy Taylor on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
