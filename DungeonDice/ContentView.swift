b953121a3ec9bf46af736b8e5a44098301f1290a
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-08 12:34:46 -0600

    Finished App

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,16 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var resultMessage = ""
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            titleView
+            
+            Spacer()
+            
+            resultMessageView
+            
+            Spacer()
+            
+            ButtonLayout(resultMessage: $resultMessage)
+            
+            
         }
         .padding()
+        
+        
     }
 }
+
+extension ContentView {
+    private var titleView: some View {
+        Text("Dungeon Dice")
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundColor(.red)
+    }
+    
+    private var resultMessageView: some View {
+        Text(resultMessage)
+            .font(.largeTitle)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
+            .frame(height: 150)
+    }
+    
+    
+    
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
