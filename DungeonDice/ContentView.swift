e26668f6fc4b8def48bd4437e301e704beb80589
Jeremy Taylor <jtaylor@jctaylor.org>
2025-05-11 13:26:48 -0500

    Layout buttons using LazyVGrid

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -40,16 +40,16 @@ struct ContentView: View {
       
       Spacer()
       
-      ForEach(Dice.allCases, id: \.self) { dice in
-        Button("\(dice.rawValue)-sided") {
-          resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
+        ForEach(Dice.allCases, id: \.self) { dice in
+          Button("\(dice.rawValue)-sided") {
+            resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+          }
         }
+        
+        .buttonStyle(.borderedProminent)
+        .tint(.red)
       }
-      
-      .buttonStyle(.borderedProminent)
-      .tint(.red)
-      
-      
     }
     .padding()
   }
